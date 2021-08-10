855dbcfd8bedc8494310c56acc094ffaa926f177
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2021-08-10 16:55:12 -0300

    Criado opções de menus diferentes para tipos de Professores

--- src/Desafio04.java
+++ src/Desafio04.java
@@ -11,6 +11,7 @@ public class Desafio04 {
 		Scanner leitor = new Scanner (System.in);
 		int aulasSemanais;
 		double horaAula;
+		int tipoProfessor =0;
 		int opcao = 0;
 		
 		System.out.println("Para inicar o programa informe o N° de Aulas semanais");
@@ -18,31 +19,35 @@ public class Desafio04 {
 		System.out.println("Digite o valor da Hora-aula");
 		horaAula = leitor.nextDouble();
 		
-		while (opcao!=5) {
-			Funcoes04.menuCalculos();
-			opcao = leitor.nextInt();
+		Funcoes04.manuProfessores();
+		tipoProfessor = leitor.nextInt();
+		
+		if (tipoProfessor == 1) {
+			while (opcao!=5) {
+				Funcoes04.menuCalculos();
+				opcao = leitor.nextInt();
 			
-			switch(opcao) {
-				case 1:
-					System.out.println("Seu salário base é R$ " + Funcoes04.salarioBase(aulasSemanais, horaAula));
-					break;
-				case 2:
-					System.out.println("O valor da sua Hora-atividade foi de R$" + Funcoes04.horaAtividade(aulasSemanais, horaAula));
-					break;
-				case 3:
-					System.out.println("Seu DSR foi R$" + Funcoes04.descansoRemunerado(aulasSemanais, horaAula));
-					break;
-				case 4:
-					System.out.println("Salário total: R$" + Funcoes04.salarioTotal(aulasSemanais, horaAula));
-					break;
-				case 5:
-					System.out.println("Saindo do Sistema");
-					break;
-				default:
-					System.out.println("Opção inválida, Por favor escolha outra opção");
+				switch(opcao) {
+					case 1:
+						System.out.println("Seu salário base é R$ " + Funcoes04.salarioAulista(aulasSemanais, horaAula));
+						break;
+					case 2:
+						System.out.println("O valor da sua Hora-atividade foi de R$" + Funcoes04.horaAtividade(aulasSemanais, horaAula));
+						break;
+					case 3:
+						System.out.println("Seu DSR foi R$" + Funcoes04.descansoRemunerado(aulasSemanais, horaAula));
+						break;
+					case 4:
+						System.out.println("Salário total: R$" + Funcoes04.salarioTotal(aulasSemanais, horaAula));
+						break;
+					case 5:
+						System.out.println("Saindo do Sistema");
+						break;
+					default:
+						System.out.println("Opção inválida, Por favor escolha outra opção");
 					break;
 			}
-			
+		}
 		}
 		
 		leitor.close();
--- src/Funcoes04.java
+++ src/Funcoes04.java
@@ -2,6 +2,13 @@
 public class Funcoes04 {
 
 	
+	public static void manuProfessores() {
+		System.out.println("ESCOLHA UMA DAS OPÇÕES");
+		System.out.println("1 - Professor Aulista");
+		System.out.println("2 - Professor Mensalista");
+		System.out.println("3 - Encerrar programa");
+	}
+	
 	public static void menuCalculos() {
 		System.out.println("ESCOLHA O CALCULO QUE DESEJA FAZER!");
 		System.out.println("1 - Salário base");
@@ -11,7 +18,21 @@ public class Funcoes04 {
 		System.out.println("5 - Fim do Programa");
 	}
 	
-	public static double salarioBase (int aulasSemanais, double horaAula) {
+	
+	public static double salarioMensalista (double salario) {
+		double adicional, salarioTotal=0;
+		adicional = salarioTotal * 0.05;
+		salarioTotal = salarioTotal + adicional;
+		return salarioTotal;
+	}
+	
+	public static double horaMensalista (double salario) {
+		double adicional, salarioTotal =0;
+		adicional = salarioTotal * 0.05;
+		return adicional;
+	}
+	
+	public static double salarioAulista (int aulasSemanais, double horaAula) {
 		double aulasMensais, salarioBase;
 		aulasMensais = aulasSemanais * 4.5;
 		salarioBase = aulasMensais * horaAula;
@@ -31,7 +52,7 @@ public class Funcoes04 {
 		aulasMensais = aulasSemanais * 4.5;
 		salarioBase = aulasMensais * horaAula;
 		adicional = salarioBase * 0.05;
-		dsr = salarioBase + adicional / 6;
+		dsr = (salarioBase + adicional) / 6;
 		return dsr;
 	}
 	

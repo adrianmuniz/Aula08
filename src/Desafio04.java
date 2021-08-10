e90e757c2bd2a3a82658f13bfbf8c9f3f0dde0b5
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2021-08-10 11:33:25 -0300

    Aplicando funções com argumentos

--- src/Desafio04.java
+++ src/Desafio04.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Desafio04 {
 	/* 
@@ -5,7 +6,40 @@ public class Desafio04 {
 	
 	Crie as funções que julgar necessárias para obter cada um dos valores que fazem parte do salário de um professor
 	 * */
-	public static void main(String[] args) {
+	
+	public static void calculoSalario(int aulasSemanais, double horaAula) {
+		double aulasMensais, salarioBase, horaAtividade, dsr, total;
+		//Calculo salario base
+		aulasMensais = aulasSemanais * 4.5;
+		salarioBase = aulasMensais * horaAula;
+		System.out.printf("Valor do salário base é de R$%.2f%n", salarioBase);
 		
+		//Calculo adicional hora-atividade
+		horaAtividade = salarioBase * 0.05;
+		System.out.printf("Adicional de hora-atividade R$%.2f%n", horaAtividade);
+		
+		//Calculo do DSR
+		dsr = (salarioBase / 6) + horaAtividade; 
+		System.out.printf("Valor do Descanso semanal Remunerado R$%.2f%n", dsr);
+		
+		// Salário Total
+		total = salarioBase + horaAtividade + dsr;
+		System.out.printf("Salário total de R$", total);
+	}
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner (System.in);
+		int aulasSemanais;
+		double horaAula;
+		
+		System.out.println("Bem vindo ao programa de cálculo de salário!");
+		System.out.println("Digite a quantidade de aulas semanais ministradas: ");
+		aulasSemanais = leitor.nextInt();
+		System.out.println("Informe o valor da hora-aula: ");
+		horaAula = leitor.nextDouble();
+		calculoSalario(aulasSemanais, horaAula);
+		
+		
+		leitor.close();
 	}
 }

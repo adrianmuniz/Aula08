8c61a2d1d1c71be38383bcf3a7ae71b42dfc324a
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2021-08-13 10:31:54 -0300

    Criando retorno das funções

--- src/Desafio06.java
+++ src/Desafio06.java
@@ -11,7 +11,9 @@ Supondo um jogo onde os jogadores têm 5 cartas na mão, crie funções para verific
 */
 	
 	public static int ocorrenciasIgualdades(String[] cartas) {
+		//criando um vetor para cópias do vetor original
 		String[] cartasComparadas = new String [cartas.length];
+		//usando o método clone
 		cartasComparadas = cartas.clone();
 		int quantidade = 0;
 		for (int i=0; i<cartasComparadas.length - 1; i++) {
@@ -23,14 +25,39 @@ Supondo um jogo onde os jogadores têm 5 cartas na mão, crie funções para verific
 				}
 			}
 		}
-		for(int i=0; i<cartas.length; i++) {
-			System.out.print(" " + cartas[i]);
-		}
-		System.out.println();
-		for(int i=0; i<cartas.length; i++) {
-			System.out.print(" " + cartasComparadas[i]);
-		}
 		return quantidade;
+	}
+	
+	public static boolean ehPar(String[] cartas) {
+		if (ocorrenciasIgualdades(cartas) == 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean ehTrinca(String[] cartas) {
+		if (ocorrenciasIgualdades(cartas) == 2) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean ehQuadra(String[] cartas) {
+		if (ocorrenciasIgualdades(cartas) == 3) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static String tipoJogo(String[] cartas) {
+		if (ehPar(cartas)) {
+			return "É um par";
+		} else if (ehTrinca(cartas)) {
+			return "É uma trinca";
+		}else if (ehQuadra(cartas)) {
+			return "É uma quadra";
+		}
+		return "Nenhum jogo foi identificado";
 	}
 	
 	public static void main(String[] args) {
@@ -38,7 +65,7 @@ Supondo um jogo onde os jogadores têm 5 cartas na mão, crie funções para verific
 		String[] valoresCartas = {"6","1","A","A","A"};
 		String[] naipeCartas = new String[5];
 
-		System.out.println(ocorrenciasIgualdades(valoresCartas));
+		System.out.println(tipoJogo(valoresCartas));
 	}
 
 }
